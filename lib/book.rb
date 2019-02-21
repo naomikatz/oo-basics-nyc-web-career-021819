767807a5582cdf6908b8e5d84a7dec70fd974677
Naomi Katz <naykatz@gmail.com>
2019-02-21 04:15:08 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
+  
+  attr_accessor :title 
+  attr_accessor :author
+  attr_accessor :genre 
+  
+  def page_count=(number)
+    @page_count = number
+end
 
+def page_count
+  @page_count
+end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+end
 
