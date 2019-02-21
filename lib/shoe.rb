63dc4d125f55d16a67ae004eb9abe903c69ebe60
Naomi Katz <naykatz@gmail.com>
2019-02-21 04:05:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+class Book
+  def initialize(title)
+    @title = title
+  end
+  
+  attr_accessor :title 
+  attr_accessor :author
+  attr_accessor :genre 
+  
+  def page_count=(number)
+    @page_count = number
+end
+
+def page_count
+  @page_count
+end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+end
+
